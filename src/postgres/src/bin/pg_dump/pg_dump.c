d85832fba39503929fe4aeebc01e07ed793d16b6
Rishabh Tanwar <33982749+rishabhtanwar29@users.noreply.github.com>
2022-05-11 21:52:02 +0530

    BABEL: MVU fails while restoring MS-TVF and procedure with T-SQL table-type
    This commit fixes following three issues with T-SQL table-type:
    
    1. While restoring MS-TVF, PG throws error that underlying
       "table-type does not exists", which is due to dependencies
       between function, table-type and underlying template table
       are not getting handled properly by pg_dump.
    2. While restoring procedure with one of it's argument as T-SQL
       table-type, PG's throws the same error that "table-type
       does not exists". This is again a dependency issue.
    3. We have added a new field "tsql_tabletype" to CreateStmt
       structure but and it is not getting captured by pg_dump.
    
    Fixes:
    
    1. Resolve dependency loop between MS-TVF and it's return
       type (T-SQL table-type).
    2. Add function's dependency upon template table instead of
       underlying table-type (dummy type), since template table
       should be restored first so that it's row type (table-type)
       gets created automatically and becomes available to be used
       by other functions/procedures.
    3. Set babelfishpg_tsql.restore_tsql_tabletype guc before restoring
       table-type's underlying template table in order to let backend
       recreate the correct dependencies between MS-TVF, table-type
       and underlying template table.
    
    Task: BABEL-3192
    Signed-off-by: Rishabh Tanwar <ritanwar@amazon.com>
    (cherry picked from commit eab30c702b8c0b062983a3f0db294a8149c9b447)

--- src/postgres/src/bin/pg_dump/pg_dump.c
+++ src/postgres/src/bin/pg_dump/pg_dump.c
@@ -11871,6 +11871,7 @@ dumpFunc(Archive *fout, const FuncInfo *finfo)
 	int			nconfigitems = 0;
 	const char *keyword;
 	int			i;
+	bool		is_tsql_mstvf = false;
 
 	/* Do nothing in data-only dump */
 	if (dopt->dataOnly)
@@ -12046,6 +12047,12 @@ dumpFunc(Archive *fout, const FuncInfo *finfo)
 	else
 		keyword = "FUNCTION";	/* works for window functions too */
 
+	is_tsql_mstvf = isTsqlMstvf(fout, finfo, prokind[0], proretset[0] == 't');
+
+	if (is_tsql_mstvf)
+		appendPQExpBufferStr(q,
+							 "SET babelfishpg_tsql.restore_tsql_tabletype = TRUE;\n");
+
 	appendPQExpBuffer(delqry, "DROP %s %s;\n",
 					  keyword, qual_funcsig);
 
@@ -12197,6 +12204,10 @@ dumpFunc(Archive *fout, const FuncInfo *finfo)
 	}
 
 	appendPQExpBuffer(q, "\n    %s;\n", asPart->data);
+
+	if (is_tsql_mstvf)
+		appendPQExpBufferStr(q,
+							 "RESET babelfishpg_tsql.restore_tsql_tabletype;\n");
 
 	append_depends_on_extension(fout, q, &finfo->dobj,
 								"pg_catalog.pg_proc", keyword,
@@ -15562,6 +15573,7 @@ dumpTableSchema(Archive *fout, const TableInfo *tbinfo)
 		char	   *ftoptions = NULL;
 		char	   *srvname = NULL;
 		char	   *foreign = "";
+		bool	   tsql_tabletype = isTsqlTableType(fout, tbinfo);
 
 		/*
 		 * Set reltypename, and collect any relkind-specific data that we
@@ -15695,6 +15707,10 @@ dumpTableSchema(Archive *fout, const TableInfo *tbinfo)
 								  "SELECT pg_catalog.binary_upgrade_set_next_tablegroup_default(true);\n");
 			}
 		}
+
+		if (tsql_tabletype)
+			appendPQExpBufferStr(q,
+								 "SET babelfishpg_tsql.restore_tsql_tabletype = TRUE;\n");
 
 		appendPQExpBuffer(q, "CREATE %s%s %s",
 						  tbinfo->relpersistence == RELPERSISTENCE_UNLOGGED ?
@@ -16001,6 +16017,10 @@ dumpTableSchema(Archive *fout, const TableInfo *tbinfo)
 		}
 		else
 			appendPQExpBufferStr(q, ";\n");
+
+		if (tsql_tabletype)
+			appendPQExpBufferStr(q,
+								 "RESET babelfishpg_tsql.restore_tsql_tabletype;\n");
 
 		/* Materialized views can depend on extensions */
 		if (tbinfo->relkind == RELKIND_MATVIEW)
@@ -18456,6 +18476,9 @@ getDependencies(Archive *fout)
 		else
 			/* normal case */
 			addObjectDependency(dobj, refdobj->dumpId);
+
+		/* Standalone T-SQL table-type as a function's argument or multi-statement TVF */
+		fixTsqlTableTypeDependency(fout, dobj, refdobj, deptype);
 	}
 
 	PQclear(res);
