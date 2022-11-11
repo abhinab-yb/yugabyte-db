b97699bfd6a32f3276ee5d6f9c14d971084e09eb
Avantika Dasgupta <davantik@amazon.com>
2022-11-11 15:56:57 -0800

    BABEL: Support nondeterministic collations for LIKE in CHECK constraint clause (#51)
    Utility statements such as ALTER TABLE are not planned. Due to collation like to ilike transformation logic being called in a planner hook, CHECK constraint in utlity commands do not support LIKE for nondeterministic collations.
    
    To solve this problem, we add a hook to invoke the like to ilike transformation function after the raw parsetree is transformed into an executable expression.
    
    Task: BABEL-3319
    Signed-off-by: Avantika Dasgupta davantik@amazon.com
    
    Extension PR: babelfish-for-postgresql/babelfish_extensions#684
    
    (cherry picked from commit 272231f8e169d4cd6546b3bb5ea69dfb506730e5)

--- src/postgres/src/include/catalog/heap.h
+++ src/postgres/src/include/catalog/heap.h
@@ -163,4 +163,8 @@ extern void StorePartitionBound(Relation rel, Relation parent,
 
 Node *YbCookConstraint(ParseState *pstate, Node *raw_constraint, char *relname);
 
+/* Hook for plugins to transform executable expressions in check constraint clause */
+typedef Node* (*transform_check_constraint_expr_hook_type) (Node *node);
+extern PGDLLIMPORT transform_check_constraint_expr_hook_type transform_check_constraint_expr_hook;
+
 #endif							/* HEAP_H */
