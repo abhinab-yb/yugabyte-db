353b974319095c28507bd30128d01421be4f59a4
Divyansh Chaturvedi <divchaturvedi13@gmail.com>
2023-03-26 14:56:30 +0000

    [#15976] YSQL : Mechanism to enable and disable tracing [WIP]
    Summary: Implemented functions to enable or disable tracing and check whether tracing is enabled for a particular proc with given pid.
    
    Test Plan: NA
    
    Reviewers: rvenkatesh, smishra, hbhanawat, asaha
    
    Subscribers: yql
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D23302

--- src/postgres/src/backend/utils/adt/pgstatfuncs.c
+++ src/postgres/src/backend/utils/adt/pgstatfuncs.c
@@ -2147,3 +2147,66 @@ yb_pg_stat_retrieve_concurrent_index_progress()
 
 	return progress;
 }
+
+Datum
+yb_pg_enable_tracing(PG_FUNCTION_ARGS)
+{
+	
+	int pid = PG_GETARG_INT32(0);
+	bool ok;
+
+	if(pid == 0)						/* Enable tracing for all the processes */
+	{
+		ok = SignalTracingAllProcs(1);
+	}
+	else
+	{
+		ok = SignalTracing(1, pid);
+	}
+	
+	if(!ok)
+	{
+		PG_RETURN_BOOL(false);
+	}
+
+	PG_RETURN_BOOL(true);
+}
+
+Datum
+yb_pg_disable_tracing(PG_FUNCTION_ARGS)
+{
+	int pid = PG_GETARG_INT32(0);
+	bool ok;
+
+	if(pid == 0)						/* Disable tracing for all the processes */
+	{
+		ok = SignalTracingAllProcs(0);
+	}
+	else
+	{
+		ok = SignalTracing(0, pid);
+	}
+	
+	if(!ok)
+	{
+		PG_RETURN_BOOL(false);
+	}
+	
+	PG_RETURN_BOOL(true);
+}
+
+/* Returns whether tracing is enabled for a particular pid or not */
+Datum
+is_yb_pg_tracing_enabled(PG_FUNCTION_ARGS)
+{
+	int pid = PG_GETARG_INT32(0);
+
+	if(pid == 0)
+	{
+		PG_RETURN_BOOL(false);
+	}
+	
+	bool is_tracing_enabled = CheckTracingEnabled(pid);
+	
+	PG_RETURN_BOOL(is_tracing_enabled);
+}
